6fe0d769fdd2a6f36328ff0ddae5d9b2bf7cfc62
oreo <ohsimon77@naver.com>
2023-01-01 20:23:58 +0900

    refactor : 필요없는 번역 repository 삭제

--- src/main/java/HotDeal/HotDeal/Repository/TranslateRepository.java
+++
@@ -1,8 +0,0 @@
-package HotDeal.HotDeal.Repository;
-
-import HotDeal.HotDeal.Domain.Translate;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TranslateRepository extends MongoRepository<Translate, String> {
-
-}
