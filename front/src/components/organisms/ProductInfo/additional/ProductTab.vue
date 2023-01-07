19ec040b26bbfdb5d1ac95e92c80f54b6124df24
oreo <ohsimon77@naver.com>
2023-01-08 04:52:38 +0900

    refactor : 태그 누르면 스크롤 되는 기능 추가

--- front/src/components/organisms/ProductInfo/additional/ProductTab.vue
+++ front/src/components/organisms/ProductInfo/additional/ProductTab.vue
@@ -1,7 +1,21 @@
 <template>
   <v-tabs
+    color="black"
+    grow
+  >
+    <v-tab @click="goto('scroll1')">
+      제품사진
+    </v-tab>
+    <v-tab @click="goto('scroll2')">
+      상세정보
+    </v-tab>
+    <v-tab @click="goto('scroll3')">
+      쇼핑가이드
+    </v-tab>
+  </v-tabs>
+  <!--  
+  <v-tabs
     v-model="tab"
-    justify="center"
     bg-color="transparent"
     color="black"
     grow
@@ -12,14 +26,21 @@
     >
       {{ tab }}
     </v-tab>
-  </v-tabs>
-  <div class="py-3">
+  </v-tabs>-->
+  <div
+    ref="scroll1"
+    class="py-3"
+  >
     <v-img src="https://m.media-amazon.com/images/W/WEBP_402378-T2/images/S/aplus-media-library-service-media/baf925ef-8d72-4b5e-9f8e-45268757be6f.__CR0,0,1464,600_PT0_SX1464_V1___.jpg" />
     <v-img src="https://m.media-amazon.com/images/W/WEBP_402378-T2/images/S/aplus-media-library-service-media/78ec524a-3178-4a16-bd7c-94492654f62e.__CR0,0,1464,600_PT0_SX1464_V1___.jpg" />
     <v-img src="https://m.media-amazon.com/images/W/WEBP_402378-T2/images/S/aplus-media-library-service-media/0b1a01a0-d5c2-465a-941d-cd8e0e17b04c.__CR0,0,1464,600_PT0_SX1464_V1___.jpg" />
   </div>
-  <ProductDetail />
-  <ShoppingGuide />
+  <div ref="scroll2">
+    <ProductDetail /> 
+  </div>
+  <div ref="scroll3">
+    <ShoppingGuide />
+  </div>
 </template>
 
 <script>
@@ -42,5 +63,20 @@ export default {
       ],
     }
   },
+  methods: {
+    goto(refName) {
+      var element = this.$refs[refName];
+      var moveTo = element.offsetTop-50;
+      //moveEle.scrollIntoView({block: "start", behavior: "smooth" });
+      window.scrollTo({top : moveTo, behavior: "smooth"});
+    }
+  }
 }
 </script>
+<style>
+.div {
+  height: 1000px;
+  padding: 20px;
+  border: 2px solid yellow;
+}
+</style>
