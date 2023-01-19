3d18bae1d32205c931cb7d212bb1525944cc814b
Jungwon423 <44633262+Jungwon423@users.noreply.github.com>
2023-01-19 17:12:04 +0900

    mobile web & pc web 분리

---
+++ front/src/mobile/organisms/Home/category/CategoryList.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col
+        v-for="categoryName in categoryNames"
+        :key="categoryName"
+        class="align-center"
+        :cols="3"
+      >
+        <CategoryButton :category-name="categoryName" />
+      </v-col>
+    </v-row>
+  </v-container>
+  <!-- <h1>{{ currentCategory }}</h1> -->
+</template>
+
+<script>
+import CategoryButton from '@mobile/molecules/CategoryButton.vue'
+
+export default {
+  name: 'CategoryList',
+
+  components: {
+    CategoryButton,
+  },
+
+  data() {
+    return {
+      categoryNames: [
+        'fashion_beaut',
+        'furniture_living',
+        'homeAppliances_computers',
+        'travel_car',
+      ],
+    }
+  },
+}
+</script>
