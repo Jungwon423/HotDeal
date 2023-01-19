f92b1b22e7893fa06366343d6cd7b911ee536c8c
Jungwon423 <jungwon423@naver.com>
2023-01-20 01:44:23 +0900

    ProductContainer + ProductSet

---
+++ front/src/web/organisms/ProductSet.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-row>
+    <v-col>
+      <product-container />
+    </v-col>
+    <v-col>
+      <product-container />
+    </v-col>
+    <v-col>
+      <product-container />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import ProductContainer from '@/web/molecules/ProductContainer.vue'
+export default {
+    name: 'ProductSet',
+    components: {
+        ProductContainer
+    }
+}
+</script>
--- front/src/web/pages/HomePage.vue
+++ front/src/web/pages/HomePage.vue
@@ -2,37 +2,40 @@
   <HomeHeader />
   <AdvertisingBox />
   <CategoryMain />
+  <product-container />
+  <product-set />
 </template>
     
   <script>
   import CategoryMain from '@/web/organisms/CategoryButtons.vue'
-    import AdvertisingBox from '@/web/organisms/AdvertisingBox.vue'
-    import HomeHeader from '@/web/organisms/HomeHeader.vue'
-    export default {
-      name: 'CategoryButtons',
-      components : {
-        HomeHeader,
-       CategoryMain,
-        AdvertisingBox,
+  import AdvertisingBox from '@/web/organisms/AdvertisingBox.vue'
+  import HomeHeader from '@/web/organisms/HomeHeader.vue'
+  import ProductContainer from '@/web/molecules/ProductContainer.vue'
+  import ProductSet from '../organisms/ProductSet.vue'
+  export default {
+    name: 'CategoryButtons',
+    components : {
+      HomeHeader,
+      CategoryMain,
+      AdvertisingBox,
+      ProductContainer,
+      ProductSet
+    },
+      
+    computed: {
+      categoryNameSub() {
+        //categoryNameSubstitution
+        return this.$categoryMap.get(this.categoryName)
       },
-      data() {
-          return {}
-      },
+    },
     
-      computed: {
-        categoryNameSub() {
-          //categoryNameSubstitution
-          return this.$categoryMap.get(this.categoryName)
-        },
+    methods: {
+      onClick: function (evt) {
+        this.$store.commit(
+          'GetProductListApi/SET_CURRENTCATEGORY',
+          this.categoryName
+        )
       },
-    
-      methods: {
-        onClick: function (evt) {
-          this.$store.commit(
-            'GetProductListApi/SET_CURRENTCATEGORY',
-            this.categoryName
-          )
-        },
-      },
-    }
+    },
+  }
   </script>
